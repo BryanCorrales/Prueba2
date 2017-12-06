b704eb6e4196d5f8537d3cc07d27b8ea43c3cf99
DESKTOP-70T0AE3\usuario <bryan.corrales@epn.edu.ec>
2017-12-06 18:30:46 -0500

    Lectura del txt y conteo de los numeros

--- README.py
+++ README.py
@@ -1,1 +1,19 @@
 # Prueba2
+def leerTexto():
+    archivo=open('datos.txt','r')
+    linea=archivo.readline()
+    print(linea)
+    lista=linea.split(' ')
+    pa=len(lista)
+    print("El numero de letras que existen en el archivo txt es",pa)
+    while linea!="":
+        linea=archivo.readline()
+    archivo.close()
+def cabecera():
+    print("Escuela Politecnica Nacional")
+    print("Prueba 2")
+    print("Integrantes: Bryan Corrales")
+    print("             Sergio Sigcha")
+    print("             Cristian Mainato")
+cabecera()
+leerTexto()
