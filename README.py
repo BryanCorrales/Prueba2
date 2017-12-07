f79fc0211264594d148650af0e7040f1fdee74e0
admin-PC\admin <christian.mainato@epn.edu.ec>
2017-12-06 22:05:08 -0500

    veanle ahi

--- README.py
+++ README.py
@@ -1,14 +1,32 @@
 # Prueba2
 def leerTexto():
-    archivo=open('datos.txt','r')
+    archivo=open('datos.txt','w')
     linea=archivo.readline()
-    print(linea)
-    lista=linea.split(' ')
-    pa=len(lista)
-    print("El numero de letras que existen en el archivo txt es",pa)
-    while linea!="":
-        linea=archivo.readline()
-    archivo.close()
+
+    def grabartxt():
+        archi = open('datoss.txt', 'w')
+        archi.write('hola 1\n')
+        archi.write('hola 2\n')
+        archi.write('como 3\n')
+        lista = []
+        lista_final = []
+
+        for numero in contenido:
+            lista += numero.split()
+        lista.sort()
+        set_lista = set(lista)
+        set_lista = list(set_lista)
+        set_lista.sort()
+        print("Valor:Ocurrencias")
+        for numero in set_lista:
+            lista_final.append((lista.count(numero), numero))
+
+        lista_final.sort(reverse=True)
+        print(lista_final)
+
+        archi.close()
+
+
 def cabecera():
     print("Escuela Politecnica Nacional")
     print("Prueba 2")
